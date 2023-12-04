4997318f25a5a5c41ab9fb4e85ef2f4d4c61623d
Marcelo Cuellar <marcelo.g.cuellar@gmail.com>
2023-12-04 01:47:39 +0000

    Added elapsed time functions to global.sh

--- linux/ubuntu_22-04/global.sh
+++ linux/ubuntu_22-04/global.sh
@@ -8,3 +8,27 @@ function log_message(){
     ts=`date '+%Y-%m-%d %H:%M:%S'`
     echo "$ts $1"
 }
+
+function save_start_time(){
+    # Record the start time
+    start_time=$(date +%s)
+}
+
+function save_end_time(){
+    # Record the start time
+    end_time=$(date +%s)
+}
+
+function display_elapsed_times(){
+    # Calculate the elapsed time in seconds
+    elapsed_seconds=$((end_time - start_time))
+
+    # Calculate the elapsed time in minutes
+    elapsed_minutes=$((elapsed_seconds / 60))
+
+    # Display the results
+    echo "Elapsed Time (seconds): $elapsed_seconds"
+    echo "Elapsed Time (minutes): $elapsed_minutes"
+}
+
+
